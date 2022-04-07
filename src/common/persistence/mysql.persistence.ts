9db2ba6836aebc1a4132f3a0985971dc6d2cd4f5
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2022-04-07 09:54:23 -0500

    refactoring the connection to sql

--- src/common/persistence/mysql.persistence.ts
+++ src/common/persistence/mysql.persistence.ts
@@ -1,9 +1,13 @@
-import { createPool } from "mysql2/promise";
-
-export default createPool({
-  host: process.env.db_mysql_host,
-  user: process.env.db_mysql_user,
-  password: process.env.db_mysql_password,
-  database: process.env.db_mysql_database,
-  decimalNumbers: true,
-});
+import { createPool } from "mysql2";
+export class MySqlPool {
+  public createMySqlPool() {
+    return createPool({
+      host: process.env.db_mysql_host as string,
+      user: process.env.db_mysql_user as string,
+      password: process.env.db_mysql_password as string,
+      database: process.env.db_mysql_database as string,
+      connectionLimit: 10,
+      decimalNumbers: true,
+    });
+  }
+}
