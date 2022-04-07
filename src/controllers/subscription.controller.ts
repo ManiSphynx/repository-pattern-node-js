e6c5737df98bb9a6fa08f23691889eae5dc2c62e
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2022-04-07 09:54:43 -0500

    controller level error handling

--- src/controllers/subscription.controller.ts
+++ src/controllers/subscription.controller.ts
@@ -5,50 +5,79 @@ import {
   SubscriptionCreateDto,
   SubscriptionUpdateDto,
 } from "../dtos/subscription.dto";
+import { BaseController } from "../common/controllers/base.controller";
 
 @route("/subscriptions")
-export class SubscriptionController {
-  constructor(private readonly subscriptionService: SubscriptionService) {}
+export class SubscriptionController extends BaseController {
+  constructor(private readonly subscriptionService: SubscriptionService) {
+    super();
+  }
   @GET()
   public async all(request: Request, response: Response) {
-    response.send(await this.subscriptionService.all());
+    try {
+      response.send(await this.subscriptionService.all());
+    } catch (error) {
+      this.handleException(error, response);
+    }
   }
 
-  @route(":id")
+  @route("/:id")
   @GET()
   public async find(request: Request, response: Response) {
-    const id = parseInt(request.params.id);
-    response.send(await this.subscriptionService.find(id));
+    try {
+      const id = parseInt(request.params.id);
+      const result = await this.subscriptionService.find(id);
+
+      if (result) {
+        response.send(result);
+      } else {
+        response.status(404).send();
+      }
+    } catch (error) {
+      this.handleException(error, response);
+    }
   }
 
   @POST()
   public async store(request: Request, response: Response) {
-    await this.subscriptionService.store({
-      user_id: request.body.user_id,
-      code: request.body.code,
-      amount: request.body.amount,
-      cron: request.body.cron,
-    } as SubscriptionCreateDto);
-    response.send();
+    try {
+      await this.subscriptionService.store({
+        user_id: request.body.user_id,
+        code: request.body.code,
+        amount: request.body.amount,
+        cron: request.body.cron,
+      } as SubscriptionCreateDto);
+      response.send();
+    } catch (error) {
+      this.handleException(error, response);
+    }
   }
 
-  @route(":id")
+  @route("/:id")
   @PUT()
   public async update(request: Request, response: Response) {
-    const id = parseInt(request.params.id);
-    await this.subscriptionService.update(id, {
-      code: request.body.code,
-      amount: request.body.amount,
-      cron: request.body.cron,
-    } as SubscriptionUpdateDto);
-    response.send();
+    try {
+      const id = parseInt(request.params.id);
+      await this.subscriptionService.update(id, {
+        code: request.body.code,
+        amount: request.body.amount,
+        cron: request.body.cron,
+      } as SubscriptionUpdateDto);
+      response.send();
+    } catch (error) {
+      this.handleException(error, response);
+    }
   }
 
-  @route(":id")
+  @route("/:id")
   @DELETE()
   public async remove(request: Request, response: Response) {
-    const id = parseInt(request.params.id);
-    await this.subscriptionService.remove(id);
-    response.send();
+    try {
+      const id = parseInt(request.params.id);
+      await this.subscriptionService.remove(id);
+      response.send();
+    } catch (error) {
+      this.handleException(error, response);
+    }
   }
 }
